85326c8ddb2d9e55cde2c29a80b5faa1b5a410d0
Bianca Soares <biannca01soares@gmail.com>
2023-10-18 16:22:10 -0300

    finalizando...

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
+# <img src="/img/icon-removebg-preview (1).png"  width="250" height="130" alt="iconPrincipal" />
+
+<p> <strong>Este é o projeto Makeup World</strong> ❤️ </p>
+
+## TEMAS
+- [Objetivos do Makeup World](#objetivos)
+- [Como executar o projeto?](#como-executar)
+- [Limitações:](#limitações)
+- [Ferramentas utilizadas no projeto:](#ferramentas-utilizadas)
+- [Link do projeto:](#link-projeto)
+
+## Objetivos do Makeup World
+<p align="justify"> Os objetivos do projeto <strong>Makeup World</strong> de Programação Web Front-End, é exibir e fornecer para o usuário a partir do consumo de dados de uma API de maquiagem, a "busca" por um item e a resposta com um leque de produtos (usando filtro!!). </p>
+
+
+## Como executar o projeto?
+<p> Primeiramente haverá uma barra de pesquisa, em que o usuário irá inserir o nome do produto que deseja, ou uma palavra-chave (ultilizando o filtro), em que o programa retornará cards com os nomes dos produtros ou associados a palavra-chave ultilizada. </p>
+
+## Limitações:
+<p> O projeto <strong>Makeup World</strong> só é capaz de retornar ao usuário os nomes dos produtos pesquisados. </p>
+
+
+## Ferramentas utilizadas:
+<p> <strong> HTML, CSS, JavaScript</strong> 🖥️ </p> 
+
+
+
+## Link do projeto:
+
+<p>A página disponivel em <a>https://github.com/biancaasoares/makeupWorld_</a></p>
 # makeupWorld
 # makeupWorld_
--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 'use strict'
+
+async function resposta() {
+    const url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+    const pesquisa = document.getElementById('pesquisa').value
+    const resultadosDiv = document.getElementById('resultados')
+    const mensagemCarregamento = document.getElementById('mensagem-carregamento')
+
+    mensagemCarregamento.style.display = 'block'
+
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json()
+
+            const resultadosLimitados = data.slice(0, 30)
+
+            const resultadosFiltrados = resultadosLimitados.filter(item => {
+                return item.name.toLowerCase().includes(pesquisa.toLowerCase())
+            })
+
+            exibirResultados(resultadosFiltrados)
+            mensagemCarregamento.style.display = 'none'
+        } else {
+            console.error("Erro na requisição. Código de status: " + response.status)
+            mensagemCarregamento.style.display = 'none'
+        }
+    } catch (error) {
+        console.error("Ocorreu um erro ao fazer a requisição:", error)
+        mensagemCarregamento.style.display = 'none'
+    }
+}
+
+function exibirResultados(resultados) {
+    const resultadosDiv = document.getElementById('resultados')
+    resultadosDiv.innerHTML = ''
+
+    if (resultados.length === 0) {
+        resultadosDiv.innerHTML = 'Nenhum resultado encontrado.'
+        return
+    }
+
+    resultados.forEach(item => {
+        const itemDiv = document.createElement('div')
+        itemDiv.textContent = item.name
+        resultadosDiv.appendChild(itemDiv)
+    })
+}
+
+document.getElementById('buscar').addEventListener('click', resposta)
